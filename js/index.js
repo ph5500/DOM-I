a789374e0b3bfff734454977f6851f45953fc522
ph5500 <philfives@gmail.com>
2020-03-09 17:22:25 -0600

    added nav bar and main title and button

--- js/index.js
+++ js/index.js
@@ -14,29 +14,74 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let aTagsNodeList = document.getElementsByTagName("a");
+let aTags = Array.prototype.slice.call(aTagsNodeList);
+
+// let index = 0;
+aTags.forEach((aTag, index) => {
+  let navItem = index + 1;
+  aTag.innerHTML = siteContent["nav"]["nav-item-" + navItem];
+  // index++;
+})
+
+// const keys = Object.keys(siteContent["nav"]);
+// ["nav-item-1", "nav-item-2", "nav-item-3"];
+// aTags.forEach((aTag, index) => {
+//   aTag.innerHTML = siteContent["nav"][keys[index]];
+// })
+
+// console.log("container: ", container);
+
+
+
+// const title = document.querySelector(".cta" ".cta-text"[h1]);
+
+// console.log("title");
+
+
+
+
+let mainHeading = document.querySelector('h1')
+mainHeading.textContent = siteContent['cta']['h1'];
+
+
+
+let mainButton = document.querySelector('button')
+mainButton.innerText = siteContent['cta']['button'];
+
+
+
+// const topTitle = document.querySelector(".cta .cta-text h1")
+// topTitle
+
+// const bottomTitleSecond = document.querySelectorforAll
+//   (".bottom-content . text-content h4")
+// bottomTitleSecond[1].textContent = siteContent["main-content"]
+// ["product-h4"]
